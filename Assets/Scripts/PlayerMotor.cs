e9f3ef3c3f8876683c626386e49807697e2ad9bd
Vishal Singh <vsalsngh@gmail.com>
2018-01-08 00:01:52 +0530

    error fixed and reuploaded the files

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+	private CharacterController controller;
+	private Vector3 moveVector;
+
+	private float speed = 5.0f;
+	private float verticalVelocity = 0.0f;
+	private float gravity = 12.0f;
+	private float animationDuration = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time < animationDuration) {
+			controller.Move (Vector3.forward * speed * Time.deltaTime);
+			return;
+		}
+
+
+		moveVector = Vector3.zero;
+		if (controller.isGrounded) {
+			
+			verticalVelocity = -0.5f;
+
+		} 
+		else {
+			verticalVelocity -= gravity * Time.deltaTime;
+		}
+
+
+
+		//X - Left and Right
+		moveVector.x = Input.GetAxisRaw("Horizontal") * speed;   
+
+
+		//Y - Up and Down
+		moveVector.y = verticalVelocity;
+
+		//Z - Forward and Backward
+		moveVector.z = speed;
+
+		controller.Move (moveVector * Time.deltaTime);
+	}
+}
